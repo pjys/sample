75c3c5dc69f3f12a386b7532f3e0e4e3af16c085
saama <hjpw237@gmail.com>
2022-05-01 11:31:20 +0900

    구조명 변경

--- src/main/java/com/pjys/sample/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pjys.sample.repository;
-
-import com.pjys.sample.model.OrderDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderDTO, String> {
-}
