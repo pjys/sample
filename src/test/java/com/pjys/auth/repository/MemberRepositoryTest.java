1db942d9c4e167905e1bb057ad3117d2ecd194b4
seyoung <seyoung0916@naver.com>
2022-05-08 16:58:56 +0900

    회원 정보 test code

---
+++ src/test/java/com/pjys/auth/repository/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.pjys.auth.repository;
+
+import com.pjys.auth.domain.Member;
+import com.pjys.auth.domain.MemberRole;
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Log
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void insertTest(){
+        for(int i=0;i<100;i++ ){
+            Member member = new Member();
+            member.setUserId("user" + i);
+            member.setUserPassword("pw" + i);
+            member.setUserEmail("user" + i +"@test.com");
+            MemberRole role = new MemberRole();
+            if(i <= 80) {
+                role.setRoleName("BASIC");
+            }else if(i <= 90) {
+                role.setRoleName("MANAGER");
+            }else {
+                role.setRoleName("ADMIN");
+            }
+            member.setRoles(Arrays.asList(role));
+            memberRepository.save(member);
+        }
+    }
+
+}
