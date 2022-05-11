8d9faa361632d0b074cd3d1f34637f8684a49c47
김정환 <jhkim8761@hanmail.net>
2022-05-11 20:27:39 +0900

    fix. Junit5 커밋

--- src/test/java/com/pjys/board/service/BoardServiceTest.java
+++ src/test/java/com/pjys/board/service/BoardServiceTest.java
@@ -4,24 +4,32 @@ import com.pjys.board.dto.BoardDTO;
 import com.pjys.board.dto.CreateBoardRequest;
 import com.pjys.board.entity.Board;
 import com.pjys.board.repository.BoardRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
 
-@SpringBootTest
-@Transactional
-@Rollback(false)
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
 class BoardServiceTest {
 
-    @Autowired
-    BoardRepository boardRepository;
+    @Mock
+    private BoardRepository boardRepository;
 
-    @Autowired
+    @InjectMocks
     private BoardService boardService;
 
     @Test
@@ -60,5 +68,30 @@ class BoardServiceTest {
         boardRequest.setUserId("lyh0208");
         boardRequest.setUserName("이연희");
         return boardRequest;
+    }
+
+    @Test
+    @DisplayName("게시판 추가 TEST")
+    void 게시판_생성_테스트_JUNIT5() {
+        /* F.I.R.S.T Unit Test  */
+        //given
+        CreateBoardRequest createBoardRequest = CreateBoardRequest();
+        Board board = createBoardEntity(createBoardRequest);
+
+        when(boardRepository.save(any())).thenReturn(board);
+        Long result = boardService.createBoard(createBoardRequest);
+
+        verify(boardRepository, times(1)).save(any());
+        assertNotNull(result);
+    }
+
+    private Board createBoardEntity(CreateBoardRequest createBoardRequest) {
+        return Board.builder()
+                .title(createBoardRequest.getTitle())
+                .category(createBoardRequest.getCategory())
+                .contents(createBoardRequest.getContents())
+                .userId(createBoardRequest.getUserId())
+                .userName(createBoardRequest.getUserName())
+                .build();
     }
 }
