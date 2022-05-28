eb2f6b045e01976ea79d1e3606dcadbd14a4e933
saama <hjpw237@gmail.com>
2022-05-28 12:37:32 +0900

    패키지 경로이동

--- src/main/java/com/pjys/ws/WebSocketEventListener.java
+++ src/main/java/com/pjys/ws/WebSocketEventListener.java
@@ -8,7 +8,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
