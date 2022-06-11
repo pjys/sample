1fe8d7c56fbde4f88acd92ee7e331f98f724fb21
bbqgo <bbgoood@nate.com>
2022-06-11 11:40:53 +0900

    CATEGORY CONVERTER 제거

--- src/main/java/com/pjys/board/entity/Category.java
+++ src/main/java/com/pjys/board/entity/Category.java
@@ -1,27 +1,6 @@
 package com.pjys.board.entity;
 
-import java.util.Arrays;
-
 public enum Category {
-    //NORMAL("1"),
-    //TASTE("2");
-
     NORMAL, TASTE;
-
-//    String value;
-//
-//    Category(String value) {
-//        this.value = value;
-//    }
-//
-//    public static Category enumOf(String n) {
-//        return Arrays.stream(Category.values())
-//                .filter(t -> t.getValue().equals(n))
-//                .findAny().orElse(null);
-//    }
-//
-//    public String getValue() {
-//        return this.value;
-//    }
 }
 
