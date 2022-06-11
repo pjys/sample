12baf3b37995ed2a5ec1002f4f8560210702a3bf
seyoung <seyoung0916@naver.com>
2022-06-11 22:26:35 +0900

    security 설정 변경

--- src/main/java/com/pjys/common/config/SecurityConfig.java
+++ src/main/java/com/pjys/common/config/SecurityConfig.java
@@ -1,42 +1,19 @@
 package com.pjys.common.config;
 
 import com.pjys.auth.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    CustomUserDetailsService customUserDetailsService;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // spring-security에서 로그인 처리 구현을 위해 주입 후 인증에 대한 처리 진행
-        // UserDetailsService를 이용해 로그인 인증 처리
-        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
-    }
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -44,41 +21,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // IFrame 동작하도록 설정
         http.headers().frameOptions().disable();
 
-        http
-                // /login에 대한 익명 액세스를 허용
-                // /admin을 ADMIN 역할로 제한 하고 다른 모든 것을 보호
-                //.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("admin")
-                .antMatchers("/**").permitAll() // 임시
-                .and().formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/login") // post로 로그인을 할 url
-                .defaultSuccessUrl("/")
-            .failureUrl("/login")
-            .and()
-            .logout();
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/", "/signup", "/login", "/css/**").permitAll()
+                .antMatchers("/member/**").authenticated() // 일반사용자 접근 가능
+                .antMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN") // 매니저, 관리자 접근 가능
+                .antMatchers("/admin/**").hasRole("ADMIN"); // 관리자만 접근 가능
+        // 인증 필요 시 로그인 페이지와 로그인 성공 시 리다이렉팅 페이지 설정 + 이메일 로그인 설정
+        http.formLogin().loginPage("/login").usernameParameter("userName").passwordParameter("userPassword").defaultSuccessUrl("/", true);
+        // 로그인 수행 uri 지정
+        http.formLogin().loginProcessingUrl("/login").defaultSuccessUrl("/", true);
+        // 인증된 사용자지만 인가된 uri가 아닌 경우 리다이렉트 설정
+        http.exceptionHandling().accessDeniedPage("/forbidden");
+        // 로그아웃
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+
+        http.userDetailsService(customUserDetailsService);
     }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-
-        web.ignoring().antMatchers("/css/**", "/images/**", "/fonts/**", "/js/**");
-    }
-
-
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/js/**","/css/**","/fonts/**","/images/**","/favicon.ico").permitAll()
-                    .antMatchers("/**").permitAll() //개발용
-                    .anyRequest().authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        ;
-    }*/
 }
